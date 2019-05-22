e4862a145b2943a93e4485666854333b186223e1
dfs <2214785325@qq.com>
2019-05-22 22:39:07 +0800

    dfs - 2019-5-22 22:38

--- route/route.php
+++ route/route.php
@@ -9,34 +9,14 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-
-Route::get('think', function () {
+Route::rule('think', function () {
     return ':)';
-});
+}, 'POST');
 
 //设置一般的路由访问格式
 //Route::rule('路由表达式','路由地址','请求类型','参数（数组）','变量规则（数组）');
-//Route::get('hello/:name', 'index/hello');
-//Route::rule('hello','index/index/test','GET|POST');
-
-
-//（切记不要传命名空间）
-//token(用post比较安全)
-Route::post('api/:version/token/user', 'api/:version.Token/getToken');
-
-//banner【轮播图】
-Route::get('api/:version/banner/:id', 'api/:version.Banner/getBanner');
-
-//theme【专题】
-Route::get('api/:version/theme','api/:version.Theme/getSimpleList');
-Route::get('api/:version/theme/:id','api/:version.Theme/getComplexOne');
-
-//product【商品】
-Route::get('api/:version/product/recent','api/:version.Product/getRecent');
-Route::get('api/:version/product/by_category','api/:version.Product/getAllInCategory');
-
-//category【分类】
-Route::get('api/:version/category/all','api/:version.Category/getAllCategories');
+Route::get('hello/:name', 'index/hello');
+Route::rule('hello', 'index/index/test', 'GET|POST');
 
 return [
 
