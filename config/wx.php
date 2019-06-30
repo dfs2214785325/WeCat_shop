7d193c3094a7484b30fb48d6cf99735df3c7694e
DeeFanSuo <35662938+dfs2214785325@users.noreply.github.com>
2019-06-30 23:35:09 +0800

    Update wx.php

--- config/wx.php
+++ config/wx.php
@@ -8,7 +8,7 @@
 
 //微信公众号
 return [
-    'app_id' => 'wx7e4f3d5c4dbfbd8d',
-    'app_secret' => 'f0ccc8bc9f706fa7fc0736eec1aa3eca',
+    'app_id' => 'wx7e4f3d2cc4dbfed0d',
+    'app_secret' => 'f0ccc8bc9f646fa7fc0736eec2ac3eca',
     'login_url' => 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code'
 ];
